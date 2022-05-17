4a58db0df67dbf5ab44ba2164767a1e15c7adfc3
Brais <braisfv22@gmail.com>
2022-05-17 20:27:34 +0200

    #042 - Hacer onClick methods for OnBoardingActivity

--- app/src/main/java/com/braiso_22/upkeep_app/usecases/onboarding/OnBoardingActivity.java
+++ app/src/main/java/com/braiso_22/upkeep_app/usecases/onboarding/OnBoardingActivity.java
@@ -3,14 +3,55 @@ package com.braiso_22.upkeep_app.usecases.onboarding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.braiso_22.upkeep_app.R;
+import com.braiso_22.upkeep_app.usecases.home.owner.OwnerHomeActivity;
+import com.braiso_22.upkeep_app.usecases.login.LoginActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OnBoardingActivity extends AppCompatActivity {
+
+    // Buttons
+    public Button ownerButton;
+    public Button employeeButton;
+    public Button adminButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+        setButtons();
+    }
+
+    public void setButtons(){
+        ownerButton = findViewById(R.id.onBoardingOwnerButton);
+        employeeButton = findViewById(R.id.onBoardingEmployeeButton);
+        adminButton = findViewById(R.id.onBoardingAdminButton);
+
+        ownerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OnBoardingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        employeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OnBoardingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OnBoardingActivity.this, OwnerHomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
