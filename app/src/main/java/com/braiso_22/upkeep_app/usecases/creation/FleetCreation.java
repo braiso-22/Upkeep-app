81e1674caec68be3ed47a9ad28069598d12d453b
Brais <braisfv22@gmail.com>
2022-05-23 23:02:36 +0200

    #063 - Hacer la activity de creación de Fleet

---
+++ app/src/main/java/com/braiso_22/upkeep_app/usecases/creation/FleetCreation.java
@@ -0,0 +1,16 @@
+package com.braiso_22.upkeep_app.usecases.creation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.braiso_22.upkeep_app.R;
+
+public class FleetCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fleet_creation);
+    }
+}
