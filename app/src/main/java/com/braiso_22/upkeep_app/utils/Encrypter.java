e20cdf964c2c8098c7518e9097b56443aceb9b7f
Brais <braisfv22@gmail.com>
2022-06-12 18:38:39 +0200

    refactor

--- app/src/main/java/com/braiso_22/upkeep_app/utils/Encrypter.java
+++ app/src/main/java/com/braiso_22/upkeep_app/utils/Encrypter.java
@@ -14,9 +14,9 @@ public class Encrypter {
     public static String encrypt(String text) throws Exception {
         SecretKeySpec key = generateKey(text);
         Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE,key);
-        byte [] encrypted = cipher.doFinal(text.getBytes());
-        return Base64.encodeToString(encrypted,Base64.DEFAULT);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encrypted = cipher.doFinal(text.getBytes());
+        return Base64.encodeToString(encrypted, Base64.DEFAULT);
     }
 
     private static SecretKeySpec generateKey(String text) throws NoSuchAlgorithmException {
